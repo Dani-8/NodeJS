ad27390b9f2383088d5604159cbff64424e9d283
dani-8 <daniyalw6190@gmail.com>
2026-04-26 21:38:49 +0500

    Updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,46 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
 function App() {
   const [serverMsg, setServerMsg] = useState("Loading...");
 
+
+  // useEffect(() => {
+  //   fetch("https://nodejs-production-7abc.up.railway.app")
+  //     .then((res) => res.text())
+  //     .then((data) => setServerMsg(data))
+  //     .catch((err) => console.error("Error:", err));
+  // }, []);
+
+  // ======================================================== 
+
   useEffect(() => {
-    fetch("https  ://nodejs-production-7abc.up.railway.app" || "http://localhost:3000")
-      .then((res) => res.text())
-      .then((data) => setServerMsg(data))
-      .catch((err) => console.error("Error:", err));
+    async function getData() {
+      const prodURL = "https://nodejs-production-7abc.up.railway.app";
+      const localURL = "http://localhost:5000";
+
+      try {
+        console.log("Trying LOCAL server...");
+        const res = await fetch(localURL);
+        const data = await res.text();
+        console.log("LOCAL server worked");
+        setServerMsg(data);
+      } catch (e) {
+        console.log("LOCAL failed, trying PROD...");
+
+        try {
+          console.log("Trying PROD server...");
+          const res = await fetch(prodURL);
+          const data = await res.text();
+          console.log("PROD server worked");
+          setServerMsg(data);
+        } catch (err) {
+          console.log("BOTH FAILED");
+          setServerMsg("Both servers failed");
+        }
+      }
+    }
+
+    getData();
   }, []);
 
   // ======================================================== 
@@ -17,7 +50,7 @@ function App() {
       <h1>My First Full-Stack</h1>
       <p>Server says: <strong>{serverMsg}</strong></p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
