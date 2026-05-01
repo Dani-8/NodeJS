9dcd92d7b9044a2ab79fdcb3320665bc6966325a
dani-8 <daniyalw6190@gmail.com>
2026-05-01 23:02:00 +0500

    Updates

---
+++ project2/frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="mt-20 text-rose-400 text-center font-black">
+        <h1>Vite + React</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
