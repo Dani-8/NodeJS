1ac8c27584966658d38c371e590dd3c4f4d70291
dani-8 <daniyalw6190@gmail.com>
2026-05-01 23:06:26 +0500

    Updates

--- project2/backend/server.js
+++ project2/backend/server.js
@@ -1,2 +1,19 @@
-console.log("Hello World!");
+const express = require('express')
+const cors = require('cors')
+  
+const app = express()
+const PORT = 5000
+// ============================================
 
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello from Node.js + Express server!' });
+})
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+})
