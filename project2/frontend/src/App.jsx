bb5686dd5fe1db3f0ba11b9eb421610481e1f03b
dani-8 <daniyalw6190@gmail.com>
2026-05-02 23:41:58 +0500

    update

--- project2/frontend/src/App.jsx
+++ project2/frontend/src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <div className="text-rose-400 text-4xl text-center font-black">
+      <div className="text-rose-400 text-4xl text-center font-black mt-14">
         <h1>Vite + React</h1>
         <p className="text-lg text-gray-600">
           this is 2nd project with backend
