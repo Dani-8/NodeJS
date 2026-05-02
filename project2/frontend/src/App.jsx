6d102927b0d9e577810d75df57eadfe610fee35f
dani-8 <daniyalw6190@gmail.com>
2026-05-02 23:42:20 +0500

    update

--- project2/frontend/src/App.jsx
+++ project2/frontend/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
       <div className="text-rose-400 text-4xl text-center font-black mt-14">
         <h1>Vite + React</h1>
         <p className="text-lg text-gray-600">
-          this is 2nd project with backend
+          This is the second project with backend!!!
         </p>
       </div>
     </>
