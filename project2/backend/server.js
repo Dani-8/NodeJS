b2683deb79e548a9a6d68598b007bf595e83b31d
dani-8 <daniyalw6190@gmail.com>
2026-05-04 00:31:10 +0500

    updates

--- project2/backend/server.js
+++ project2/backend/server.js
@@ -8,6 +8,26 @@ app.use(cors())
 app.use(express.json())
 // ============================================
 
+// Pre-filled Karachi Favorites
+let recipes = [
+  { 
+    id: 1, 
+    title: "Karachi Chicken Biryani", 
+    ingredients: "Basmati rice, Chicken, Sela rice, Aloo (potatoes), Biryani Masala, Yogurt, Dried Plums." 
+  },
+  { 
+    id: 2, 
+    title: "Anday Wala Burger", 
+    ingredients: "Shami Kabab patty, Egg, Bun, Onions, Cabbage, Spicy Green Chutney." 
+  },
+  { 
+    id: 3, 
+    title: "Doodh Patti & Paratha", 
+    ingredients: "Strong Black Tea, Milk, Sugar, Cardamom, Flaky Crispy Paratha." 
+  }
+]
+// ============================================
+
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello from Node.js + Express server!' });
