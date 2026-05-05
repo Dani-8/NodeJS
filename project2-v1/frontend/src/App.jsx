b368073d779e034f2df057f951f1b74d37d2bcd9
dani-8 <daniyalw6190@gmail.com>
2026-05-05 21:03:07 +0500

    updates

---
+++ project2-v1/frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App(){
+  return(
+    <>
+      <h1 className="text-2xl mt-25 text-rose-300">Welcome to my React App!</h1>
+      <p>This is a simple React application.</p>
+    </>
+  )
+}
+
+export default App
