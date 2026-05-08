5f48477afd8ca20d31c0178e7c419fb5555f20d1
dani-8 <daniyalw6190@gmail.com>
2026-05-08 06:55:56 +0500

    Updates

--- project-hub/src/projectsData.js
+++ project-hub/src/projectsData.js
@@ -3,7 +3,7 @@ const projects = [
       id: "project1",
       title: "Project One",
       version: "v1.0",
-      description: "Initial CRUD exploration with basic state management.",
+      description: "Backend connect with Frontend exploration with basic state management.",
       tech: ["Node.js", "React", "Express"],
       color: "from-blue-500 to-indigo-600",
       link: "https://node-js-black-xi.vercel.app/"
@@ -11,18 +11,18 @@ const projects = [
     {
       id: "project2-v1",
       title: "Tasks Manager",
-      version: "v2.2",
+      version: "v2.1",
       description: "A simple Task Management system with clean architecture.",
-      tech: ["Lucide", "Express", "React Hooks"],
+      tech: ["React", "Express", "Lucide", "Tailwind"],
       color: "from-rose-500 to-orange-600",
       link: "https://node-js-zoq6.vercel.app/"
     },
     {
       id: "project2-v2",
       title: "KitchenVault",
-      version: "v2.1",
+      version: "v2.2",
       description: "Advanced Recipe Management with persistent logic documentation.",
-      tech: ["Tailwind", "REST API", "Vite"],
+      tech: ["Tailwind", "REST API", "Vite", "Lucide", "React", "Express"],
       color: "from-emerald-500 to-teal-600",
       link: "https://node-js-kn2n.vercel.app/"
     }
